ebb0c48a07f2327a0f2453433ef099b400420d82
Veggie <cmderochie+github@gmail.com>
2016-11-26 16:23:29 -0700

    Initial commit

---
+++ Ataxx/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Ataxx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(273, 258);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.VirtualMode = true;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            this.dataGridView1.CellValueNeeded += new System.Windows.Forms.DataGridViewCellValueEventHandler(this.dataGridView1_CellValueNeeded);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 30;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "2";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 30;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "3";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 30;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "4";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 30;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "5";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 30;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "6";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 30;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "7";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 30;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(293, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(43, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 310);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Ataxx/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ataxx
+{
+    public partial class Form1 : Form
+    {
+        const int N = 5500000;
+        enum CellState
+        {
+            Empty,
+            Human,
+            Computer
+        }
+
+        CellState[,] _grid = new CellState[7, 7];
+        CellState[][,] _grids = new CellState[N][,];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            init();
+        }
+
+        void init()
+        {
+            _grid[0, 0] = CellState.Human;
+            _grid[6, 6] = CellState.Human;
+            _grid[0, 6] = CellState.Computer;
+            _grid[6, 0] = CellState.Computer;
+
+            foreach (int i in Enumerable.Range(0, N))
+            {
+                _grids[i] = new CellState[7, 7];
+            }
+        }
+
+        int _next = 0;
+        object _locker = new object();
+        CellState[,] clone(CellState[,] grid)
+        {
+            int next;
+            lock (_locker) next = _next++;
+            if (next >= N)
+                return null;
+            var result = _grids[next];
+            Array.Copy(grid, result, 49);
+            return result;
+        }
+
+        void makeMove(CellState[,] grid, int x1, int y1, int x2, int y2)
+        {
+            CellState orig = grid[x1, y1];
+            switch (orig)
+            {
+                case CellState.Empty:
+                    break;
+                default:
+                    if (Math.Abs(x2 - x1) > 1 || Math.Abs(y2 - y1) > 1)
+                        grid[x1, y1] = CellState.Empty;
+                    placePiece(grid, orig, x2, y2);
+                    break;
+            }
+        }
+        
+        void placePiece(CellState[,] grid, CellState play, int x, int y)
+        {
+            CellState opponent = (play == CellState.Computer) ? CellState.Human : CellState.Computer;
+
+            grid[x, y] = play;
+            for (int xx = x - 1; xx <= x + 1; xx++)
+            {
+                for (int yy = y - 1; yy <= y + 1; yy++)
+                {
+                    if (xx < 0 || xx > 6 || yy < 0 || yy > 6)
+                        continue;
+                    if (grid[xx, yy] == opponent)
+                        grid[xx, yy] = play;
+                }
+            }
+        }
+
+        int score(CellState[,] grid, CellState player)
+        {
+            return Enumerable.Range(0, 7).Sum(x => Enumerable.Range(0, 7).Sum(y => (grid[x, y] == CellState.Empty) ? 0 : (grid[x, y] == player) ? 1 : -1));
+        }
+
+        IEnumerable<Tuple<Point, Point>> validPlays(CellState[,] grid, CellState player)
+        {
+            for (int x = 0; x < 7; x++)
+            {
+                for (int y = 0; y < 7; y++)
+                {
+                    if (grid[x,y] == CellState.Empty)
+                    {
+                        bool done = false;
+                        for (int xx = x - 2; !done && xx <= x + 2; xx++)
+                        {
+                            for (int yy = y - 2; yy <= y + 2; yy++)
+                            {
+                                if (xx < 0 || xx > 6 || yy < 0 || yy > 6)
+                                    continue;
+                                if (grid[xx, yy] == player)
+                                {
+                                    yield return new Tuple<Point, Point>(new Point(xx, yy), new Point(x, y));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Tuple<Point, Point, int> bestMove(CellState[,] grid, CellState player, CellState opponent, int lookAhead, bool parallel = false)
+        {
+            var valid = validPlays(grid, player).ToList();
+
+            Func<Tuple<Point,Point>,Tuple<Point,Point,int>> select = move => new Tuple<Point, Point, int>(move.Item1, move.Item2, testMove(grid, move, player, opponent, lookAhead));
+            var tests = parallel ? valid.AsParallel().Select(select).ToList() : valid.Select(select).ToList();
+            return tests.OrderByDescending(t => t.Item3).FirstOrDefault();
+        }
+
+        int testMove(CellState[,] grid, Tuple<Point, Point> move, CellState player, CellState opponent, int lookAhead)
+        {
+            var newGrid = clone(grid);
+            if (newGrid == null)
+                return score(grid, player);
+            makeMove(newGrid, move.Item1.X, move.Item1.Y, move.Item2.X, move.Item2.Y);
+            if (lookAhead > 0)
+            {
+                var opponentMove = bestMove(newGrid, opponent, player, lookAhead - 1);
+                if (opponentMove == null)
+                    return int.MaxValue;
+                return -opponentMove.Item3;
+            }
+
+            return score(newGrid, player);
+        }
+
+        int LookAhead { get { return int.Parse(textBox1.Text); } }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _next = 0;
+            var move = bestMove(_grid, CellState.Human, CellState.Computer, LookAhead, true);
+            makeMove(_grid, move.Item1.X, move.Item1.Y, move.Item2.X, move.Item2.Y);
+
+            dataGridView1.Invalidate();
+            dataGridView1.Update();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.AddRange(Enumerable.Range(0, 7).Select(r => new DataGridViewRow()).ToArray());
+        }
+
+        private void dataGridView1_CellValueNeeded(object sender, DataGridViewCellValueEventArgs e)
+        {
+            e.Value = (_grid[e.ColumnIndex, e.RowIndex] == CellState.Computer) ? "O" : (_grid[e.ColumnIndex, e.RowIndex] == CellState.Human) ? "X" : "";
+        }
+
+        Point _from;
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                _from = new Point(e.ColumnIndex, e.RowIndex);
+            else if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                makeMove(_grid, _from.X, _from.Y, e.ColumnIndex, e.RowIndex);
+                dataGridView1.Invalidate();
+                dataGridView1.Update();
+            }
+        }
+
+        IEnumerable<Tuple<Point, Point, CellState[,]>> nextGrids(CellState[,] grid, CellState player, CellState opponent, Tuple<Point, Point> orig)
+        {
+            return validPlays(grid, player).Select(move =>
+            {
+                var m = orig ?? move;
+                var newGrid = clone(grid);
+                makeMove(newGrid, move.Item1.X, move.Item1.Y, move.Item2.X, move.Item2.Y);
+                return new Tuple<Point, Point, CellState[,]>(m.Item1, m.Item2, newGrid);
+            });
+        }
+    }
+}
